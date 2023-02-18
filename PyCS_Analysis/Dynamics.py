335092aee1d0bcd5ff4673343f18a74202a2a2e7
Eliza Diggins (Linux) <u1281896@utah.edu>
2023-02-18 16:43:30 -0700

    fixed dynamical seeking algorithm

--- PyCS_Analysis/Dynamics.py
+++ PyCS_Analysis/Dynamics.py
@@ -483,7 +483,7 @@ def mp_get_centers(output_paths: list, temp_directory: str, resolution, width, f
             log_print("The distance between the two clusters is %s, we are setting the next image width at 2.1*w = %s"%(np.sqrt(np.sum(np.array(true_x)**2+np.array(true_y)**2)),int(2.1*np.sqrt(np.sum(np.array(true_x)**2+np.array(true_y)**2)))),
                       fdbg_string,"debug")
 
-            set_width = pyn.units.Unit("%s kpc"% int(2.1*np.sqrt(np.sum(np.array(true_x)**2+np.array(true_y)**2))))
+            set_width = pyn.units.Unit("%s kpc"% int(2.5*np.sqrt(np.sum(np.array(true_x)**2+np.array(true_y)**2))))
         else:
             pass
 
